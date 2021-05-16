1561a98b2b3d084a6e571846b12abfde0aef327b
ukhan7573 <57367277+ukhan7573@users.noreply.github.com>
2021-05-16 13:08:47 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Name: Ubaid Ali Khan \n Email: ukhan7573@gmail.com"),
+      ),
+      
+    );
+  }
+}
+
+
+
